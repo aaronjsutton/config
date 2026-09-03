e2a54f8d12559a3f8cc71d0d0aa9319c450115a4
Aaron Sutton <asutton@roadrunnerwm.com>
2026-09-03 14:18:30 -0400

    add nix-command flakes

--- setup.sh
+++ setup.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
 curl --proto '=https' --tlsv1.2 -L https://nixos.org/nix/install | sh -s -- --daemon
+
+mkdir -p ~/.config/nix
+echo "experimental-features = nix-command flakes" >> ~/.config/nix/nix.conf
+
+# nix run home-manager/release-26.05 -- switch --flake .#coder
+# home-manager switch --flake .#codermkdir -p ~/.config/nix
